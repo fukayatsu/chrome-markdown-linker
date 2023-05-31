6af898ea09ef8868fa7cc4d7593ec76710a3c740
Gabriel Cornejo <gcornejo441@gmail.com>
2023-05-30 17:00:53 -0700

    initial commit

--- src/content_script.js
+++ src/content_script.js
@@ -1,34 +1,49 @@
-window.addEventListener('keydown', function(event) {
-  // Command + Ctrl + C
-  if (!(event.keyCode == 67 && event.metaKey && event.ctrlKey)) { return; }
-  chrome.extension.sendMessage({ action: "copyPage" });
-}, true);
+(() => {
+  chrome.runtime.sendMessage({ action: "copyPage" });
 
-document.addEventListener("contextmenu", function (event) {
-  var selection = window.getSelection();
+  chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
+    log("Got message from background page: " + msg);
+  });
+  
+})()
 
-  var linksInSelection = 0;
-  var text;
-  if (selection.rangeCount == 0) {
-    text = $(event.target).text() || $(event.target).parent().text()
-  } else {
-    var clonedSelection = selection.getRangeAt(0).cloneContents();
-    var div = document.createElement('div');
-    div.appendChild(clonedSelection);
-    linksInSelection = $('a[href]', div);
-  }
+// window.addEventListener('keydown', function(event) {
+//   // Command + Ctrl + C
+//   if (!(event.keyCode == 67 && event.metaKey && event.ctrlKey)) { return; }
+//   chrome.runtime.sendMessage({ action: "copyPage" });
+// }, true);
 
-  if (linksInSelection.length > 0) {
-    links = [];
-    linksInSelection.each(function() {
-      links.push({
-        href: $(this).prop('href'),
-        text: $(this).text(),
-        src:  $(this).find('img').attr('src')
-      });
-    });
-    chrome.extension.sendMessage({ action: "copySelection", links: links });
-  } else {
-    chrome.extension.sendMessage({ action: "copyElement", text: text });
-  }
-}, true);
+// console.log(`***************: CMD + CRTL + C is pressed`)
+
+// document.addEventListener("contextmenu", function (event) {
+//   var selection = window.getSelection();
+
+//   var linksInSelection = 0;
+//   var text;
+//   if (selection.rangeCount == 0) {
+//     console.log(`***************: ${selection.rangeCount} is ZERO`)
+//     text = $(event.target).text() || $(event.target).parent().text()
+//   } else {
+//     console.log(`***************: ${selection.rangeCount}`)
+//     var clonedSelection = selection.getRangeAt(0).cloneContents();
+//     var div = document.createElement('div');
+//     div.appendChild(clonedSelection);
+//     linksInSelection = $('a[href]', div);
+//   }
+
+//   if (linksInSelection.length > 0) {
+//     console.log(`***************LinksInSelection: ${linksInSelection.length}`)
+
+//     links = [];
+//     linksInSelection.each(function() {
+//       links.push({
+//         href: $(this).prop('href'),
+//         text: $(this).text(),
+//         src:  $(this).find('img').attr('src')
+//       });
+//     });
+//     chrome.runtime.sendMessage({ action: "copySelection", links: links });
+//   } else {
+//     chrome.runtime.sendMessage({ action: "copyElement", text: text });
+//   }
+// }, true);
